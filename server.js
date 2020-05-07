9a26b9e7e13218f0e56673b737f93355c16d36f5
PavelZaika <zpk.loki@gmail.com>
2020-05-07 17:33:38 +0300

    Backend Routes

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.json({ msg: 'Hello World!' }));
+
+//Define Routes
+app.use(`/api/users`, require(`./routes/users`));
+app.use(`/api/auth`, require(`./routes/auth`));
+app.use(`/api/contacts`, require(`./routes/contacts`));
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
