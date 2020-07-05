218020dd7a974c0907dd5b4b6415b0ae966bc8e9
PavelZaika <zpk.loki@gmail.com>
2020-07-05 16:44:41 +0300

    config cleaned

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Connect DB
+//Connect DataBase
 connectDB();
 app.get('/', (req, res) => res.json({ msg: 'Hello World!' }));
 
