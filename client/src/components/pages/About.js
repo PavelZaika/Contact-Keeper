0196f01d251df27a45e18a9fd565b23b958fdb1d
PavelZaika <zpk.loki@gmail.com>
2020-08-01 18:10:34 +0300

    Cleanup, Navbar, Router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping contacts
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>
+      <p className='my-1'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        consectetur repellat, facilis nesciunt labore similique. Corporis,
+        temporibus. Dolore, culpa. Voluptatum, obcaecati similique ex laboriosam
+        aut, cum vitae amet est perferendis cupiditate quaerat, commodi minus a
+        inventore quis dolore? Id ad recusandae hic tempora quibusdam ea at
+        rerum omnis optio nisi quae exercitationem eos repellat in accusantium
+        necessitatibus veritatis commodi eveniet, facere ipsa vitae itaque
+        perferendis quia. Officiis optio quo, sint dolore laborum explicabo
+        commodi deleniti! In quae error dolorem id obcaecati iusto pariatur
+        eaque fuga nostrum modi atque, minus ad tenetur. Minus magnam amet nobis
+        vel odit vitae cum in!
+      </p>
+    </div>
+  );
+};
+
+export default About;
